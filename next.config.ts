5658548ffeddf27997c1994fe11cb49e13575c74
ilyesbensari-hue <ilyes.bensari@gmail.com>
2026-01-28 22:24:10 +0100

    fix: disable Sentry in CI to prevent build failures
    - Conditionally disable Sentry integration when CI env var is set
    - Prevents build failures due to missing Sentry credentials
    - Sentry still works in local dev and production deployments

--- next.config.ts
+++ next.config.ts
@@ -1,59 +1,41 @@
 import * as Sentry from "@sentry/nextjs";
 
-export default Sentry.withSentryConfig(
-  /** @type {import('next').NextConfig} */
-  {
-    images: {
-      formats: ['image/webp', 'image/avif'],
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'images.unsplash.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'res.cloudinary.com',
-        },
-      ],
-      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
-      imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-      minimumCacheTTL: 60,
-    },
+const nextConfig = {
+  images: {
+    formats: ['image/webp', 'image/avif'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60,
   },
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
+};
 
-    org: "achrilik",
-    project: "achrilik-web",
-
-    // Only print logs for uploading source maps in CI
-    silent: !process.env.CI,
-
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Automatically annotate React components to show their full name in breadcrumbs and session replay
-    reactComponentAnnotation: {
-      enabled: true,
-    },
-
-    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    // This can increase your server load as well as your hosting bill.
-    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-    // side errors will fail.
-    tunnelRoute: "/monitoring",
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-  }
-);
+// Disable Sentry in CI environment to avoid build failures
+if (process.env.CI) {
+  module.exports = nextConfig;
+} else {
+  module.exports = Sentry.withSentryConfig(
+    nextConfig,
+    {
+      org: "achrilik",
+      project: "achrilik-web",
+      silent: !process.env.CI,
+      widenClientFileUpload: true,
+      reactComponentAnnotation: {
+        enabled: true,
+      },
+      tunnelRoute: "/monitoring",
+      disableLogger: true,
+      automaticVercelMonitors: true,
+    }
+  );
+}
